1e896ea1bf6bb36a57d7658feed15d777b59768f
jjan <declercqjan@gmail.com>
2023-07-23 19:00:51 +0200

    trying out code generator

--- testdrive/protected/config/database.php
+++ testdrive/protected/config/database.php
@@ -4,11 +4,10 @@
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
-	'charset' => 'utf8',
-	*/
+	// todo: checken of dit effectief werkt (moet ik iets runnen?) want merk nog geen verschil. edit: als ik dan later naar http://localhost:8500/index.php?r=gii/default/index ga, faalt het dus toch effect
+//	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+//	'emulatePrepare' => true,
+//	'username' => 'root',
+//	'password' => '',
+//	'charset' => 'utf8',
 );
