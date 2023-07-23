d776dd6ebe1758e4e24c772837521761242a091d
jjan <declercqjan@gmail.com>
2023-07-23 16:42:27 +0200

    composer create-project --prefer-dist --no-dev --stability=dev yiisoft/yii yii-1
    https://www.yiiframework.com/doc/guide/1.1/en/quickstart.installation
    https://www.yiiframework.com/download
    https://packagist.org/packages/yiisoft/yii

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yiisoft/yii',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.1.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'yiisoft/yii' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.1.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
